ff1d6bc4c5d50f6fabb7484a7e68f32851e49149
Fonkou Nixon <78366852+FNMALIC@users.noreply.github.com>
2024-05-20 03:29:34 +0100

    valide avif

--- lib/actions.ts
+++ lib/actions.ts
@@ -1,7 +1,7 @@
 "use server";
 import { revalidatePath } from "next/cache";
 import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
-import sharp from "sharp";
+import Jimp from "jimp";
 
 const s3Client = new S3Client({
   region: process.env.DB_NEXT_AWS_BUCKET_REGION,
@@ -20,11 +20,19 @@ async function uploadFileToS3(file, fileName, contentType) {
       ContentType: contentType,
     };
 
+    // Ensure headers are strings
+    for (const headerName in params) {
+      if (typeof params[headerName] !== 'string') {
+        params[headerName] = String(params[headerName]);
+      }
+    }
+
     const command = new PutObjectCommand(params);
     const response = await s3Client.send(command);
     console.log("File uploaded successfully:", response);
     return fileName;
   } catch (error) {
+    console.error("Error uploading to S3:", error);
     throw error;
   }
 }
@@ -45,22 +53,19 @@ export async function uploadFile(formData) {
 
     // Check if the file is an AVIF image and convert if necessary
     if (contentType === 'image/avif') {
-      const image = sharp(buffer);
+      const image = await Jimp.read(buffer);
       processedBuffer = await image
         .resize(800, 400)
-        .jpeg({ quality: 50 })
-        .toBuffer();
+        .quality(50) // Adjust the quality of the JPEG output
+        .getBufferAsync(Jimp.MIME_JPEG);
       fileName = fileName.replace(/\.[^/.]+$/, ".jpg"); // Change the file extension to .jpg
       contentType = "image/jpeg"; // Set the content type to image/jpeg
     } else {
-      const image = sharp(buffer);
+      const image = await Jimp.read(buffer);
       processedBuffer = await image
         .resize(800, 400)
-        .toBuffer({ resolveWithObject: true })
-        .then(({ data, info }) => {
-          contentType = `image/${info.format}`;
-          return data;
-        });
+        .getBufferAsync(Jimp.AUTO); // This will automatically set the format
+      contentType = Jimp.AUTO;
     }
 
     await uploadFileToS3(processedBuffer, fileName, contentType); // Upload the file to S3
@@ -82,11 +87,19 @@ export async function uploadEditorFile(content, fileName) {
       ContentType: "text/plain", // Set the content type to plain text
     };
 
+    // Ensure headers are strings
+    for (const headerName in params) {
+      if (typeof params[headerName] !== 'string') {
+        params[headerName] = String(params[headerName]);
+      }
+    }
+
     const command = new PutObjectCommand(params);
     const response = await s3Client.send(command);
 
     return response;
   } catch (error) {
+    console.error("Error uploading editor file to S3:", error);
     throw error;
   }
 }
